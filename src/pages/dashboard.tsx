84b87ed2fbaf49183b9695336a2ed28f7758d6f8
Stavros Thalassinos <stavros8003@gmail.com>
2024-05-20 23:34:10 +0300

    dashboard changes

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,36 @@
-import {useAuth} from "@/context/AuthContext";
+import { useAuth } from "@/context/AuthContext";
 import Navbar from "@/components/Navbar";
-import {useRouter} from "next/router";
 import RequireAuth from "@/components/RequireAuth";
+import { Container, Typography, Avatar, Button } from "@mui/material";
 
 function Dashboard() {
     const { user } = useAuth();
-    const router = useRouter();
 
     return (
-        <div>
+        <>
             <Navbar />
-            <h1>Hello, {user?.name}</h1>
-        </div>
-    )
+            <Container>
+                <Container className="p-0 m-0 mt-4 flex justify-between">
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        Welcome, {user?.name}
+                    </Typography>
+                    {/*<Avatar alt={user?.name} src={user?.profilePicture} />*/}
+                </Container>
+                <Typography variant="h6" component="h2" gutterBottom className="mt-10">
+                    Account Balance:
+                </Typography>
+
+                <Container className="p-8 m-0 mb-10 border-2 rounded-2xl shadow-xl">
+                    <Typography variant="h1" component="h2">
+                        $ {user?.accountBalance}
+                    </Typography>
+                </Container>
+                <Button variant="contained" size={"large"} className="rounded-full" color="primary">
+                    + Add Money
+                </Button>
+            </Container>
+        </>
+    );
 }
 
 export default RequireAuth(Dashboard);
